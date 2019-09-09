9f1ad0aea14d8fbfceee0fc4d25403bf616fdacd
Arindo-Mudiare <arindoTmudiare@gmail.com>
2019-09-09 14:32:51 +0100

    set up routing with RouteDispatcher class

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -2,20 +2,7 @@
 
 $router = new AltoRouter();
 
-// map homepage
-// $router->map('GET', '/', function () {
-//     require __DIR__ . '/views/home.php';
-// });
-$router->map('GET', '/', '', 'home');
 
-$match = $router->match();
+$router->map('GET', '/', 'App\Controllers\IndexController@show', 'home');
 
-if ($match) {
-    require_once __DIR__ . '/../controllers/BaseController.php';
-    require_once __DIR__ . '/../controllers/IndexController.php';
-    $index = new \App\Controllers\IndexController();
-    $index->show();
-} else {
-    header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
-    echo 'fresh toto Page not found';
-}
+
