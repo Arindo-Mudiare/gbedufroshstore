f7866c280cc101c6bb8260689af95b6cc75986fc
Arindo-Mudiare <arindoTmudiare@gmail.com>
2019-09-10 13:50:59 +0100

    sucessfully set up eloqunt ORM

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -8,6 +8,12 @@ if (!isset($_SESSION)) session_start();
 
 require_once __DIR__ . '/../app/config/_env.php';
 
+require_once __DIR__ . '/../app/classes/Database.php';
+
+//instantiate database class
+new \App\Classes\Database();
+
+//Load application routes
 require_once __DIR__ . '/../app/routing/routes.php';
 
 new \App\RouteDispatcher($router);
--- public/index.php
+++ public/index.php
@@ -3,3 +3,9 @@
 require_once __DIR__ . '../../bootstrap/init.php';
 
 $app_name = getenv('APP_NAME');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$category = Capsule::table('categories')->where('id', 1)->first();
+
+var_dump($category);
